f02a2f94c98d5b2c638fe1f1d12f5d5498c5085c
Anju Krishnan <anju@anjus-mbp.home>
2020-01-20 12:11:58 +0000

    adding txt file

--- txttospeech.py
+++ txttospeech.py
@@ -1,21 +1,24 @@
 #!/Users/anju/Documents/anju/learning/dciv2-code/mycode3/bin/python
 
+#pyttsx3 is a text-to-speech conversion library in Python. 
+#Unlike alternative libraries, it works offline, and is compatible with both Python 2 and 3.
+
 import pyttsx3
 
-engine = pyttsx3.init()
-fh = open("/Users/anju/Documents/anju/learning/dciv2-code/txttospeech.txt","r")
+engine = pyttsx3.init() # object creation
 
+#open the text file in read mode
+fh = open("/Users/anju/Documents/anju/gitpycodes/pylearning/txttospeech.txt","r")
 
-#contents = fh.read()
-#engine.say(contents)
-#print(contents)
-#engine.runAndWait()
-
+#save the lines in a list to read the file line by line
 lines = fh.readlines()
+
+#to parse through the list and say the lines loud
 for x in lines:
     engine.say(x)
     print(x)
     engine.runAndWait()
-    
+
+#close the file    
 fh.close()
 print("Thank you")
